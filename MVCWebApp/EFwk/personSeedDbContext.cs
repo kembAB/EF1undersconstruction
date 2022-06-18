93db7602b0d01380dd5a2f628ed07fd3ac748bcf
abel magicho <32877794+kembAB@users.noreply.github.com>
2022-06-19 01:32:22 +0200

    first entity framework

---
+++ MVCWebApp/EFwk/personSeedDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCWebApp.Models.Person;
+
+
+namespace MVCWebApp.EFwk
+{
+    public class personSeedDbContext:DbContext
+    {
+        public personSeedDbContext(DbContextOptions<personSeedDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<personproperties> People { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<personproperties>().HasData(new personproperties { ID = 1, Name = "John Stwart", City = "Lund", PhoneNumber = "0786574567" });
+            modelBuilder.Entity<personproperties>().HasData(new personproperties { ID = 2, Name = "Josefine Gustafsson", City = "Gothenburg", PhoneNumber = "0786544567" });
+            modelBuilder.Entity<personproperties>().HasData(new personproperties { ID = 3, Name = "Andrew  Monnet", City = "Stockholm", PhoneNumber = "0786894567" });
+        }
+    }
+}
---
+++ MVCWebApp/Models/Person/Personpopulate.cs
@@ -0,0 +1,112 @@
+﻿using MVCWebApp.EFwk;
+using MVCWebApp.Models.Person.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWebApp.Models.Person
+{
+    public class Personpopulate:Iperson
+    {
+        private readonly personSeedDbContext _context;
+
+        public Personpopulate(personSeedDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<personproperties> GetAllPersons()
+        {
+            return _context.People.ToList();
+        }
+
+        public personproperties GetPerson(int id)
+        {
+            return _context.People.Find(id);
+        }
+
+        public List<personproperties> Search(string searchTerm, bool caseSensitive)
+        {
+            List<personproperties> searchList = new List<personproperties>();
+
+            if (searchTerm != null)
+            {
+                if (caseSensitive)
+                {
+                    IEnumerable<personproperties> searchList2 = from Person in _context.People
+                                                      where Person.Name.Contains(searchTerm) || Person.City.Contains(searchTerm)
+                                                      select Person;
+
+                    //cheat case sensitive
+                    foreach (personproperties item in searchList2)
+                    {
+                        if (item.Name.Contains(searchTerm) || item.City.Contains(searchTerm))
+                        {
+                            searchList.Add(item);
+                        }
+                    }
+                }
+                else
+                {
+                    searchList = _context.People.Where(p => p.City.Contains(searchTerm) ||
+                                                    p.Name.Contains(searchTerm)).ToList();
+                }
+            }
+
+            return searchList;
+        }
+
+        public List<personproperties> Sort(PersonReorderVIewModel sortOptions, string sortType)
+        {
+            
+            List<personproperties> sortedList = _context.People.ToList();
+
+            if (sortType == "city")
+            {
+                sortedList = _context.People.OrderBy(p => p.City).ToList();
+            }
+            else if (sortType == "name")
+            {
+                sortedList = _context.People.OrderBy(p => p.Name).ToList();
+            }
+
+            if (sortOptions.ReverseAplhabeticalOrder == true)
+            {
+                sortedList.Reverse();
+            }
+
+            return sortedList;
+        }
+
+        public personproperties Add(CreatePersonViewModel createPersonViewModel)
+        {
+            personproperties person = new personproperties();
+            person.Name = createPersonViewModel.Name;
+            person.City = createPersonViewModel.City;
+            person.PhoneNumber = createPersonViewModel.PhoneNumber;
+
+            _context.People.Add(person);
+            _context.SaveChanges();
+
+            return person;
+        }
+
+        public bool Delete(int id)
+        {
+            if (id > 0)
+            {
+                var personToDelete = _context.People.Find(id);
+
+                if (personToDelete != null)
+                {
+                    _context.People.Remove(personToDelete);
+                    _context.SaveChanges();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
